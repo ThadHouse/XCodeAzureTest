031587c2121294ec79f67e17ab42465c356bebeb
Thad House <thadhouse1@gmail.com>
2019-05-05 10:08:40 -0700

    Add Azure Stuff

---
+++ ObjCppFile.mm
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+
+@interface XYZPerson : NSObject
+- (void)sayHello;
+@end
+
+
+@implementation XYZPerson
+- (void)sayHello {
+    NSLog(@"Hello, World!");
+}
+@end
+
+
+int main() {
+
+}
